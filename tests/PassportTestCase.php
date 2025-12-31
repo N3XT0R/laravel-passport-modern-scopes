80d9f8fe69bba4348dc42bf8bdc49d30f2a63031
Ilya Beliaev <info@php-dev.info>
2025-12-31 13:46:25 +0100

    initial commit.

--- tests/PassportTestCase.php
+++ tests/PassportTestCase.php
@@ -4,7 +4,30 @@ declare(strict_types=1);
 
 namespace N3XT0R\PassportModernScopes\Tests;
 
-class PassportTestCase
-{
+use Laravel\Passport\Passport;
 
+class PassportTestCase extends DatabaseTestCase
+{
+    const string KEYS = __DIR__ . '/../keys';
+    const string PUBLIC_KEY = self::KEYS . '/oauth-public.key';
+    const string PRIVATE_KEY = self::KEYS . '/oauth-private.key';
+
+    protected function setUp(): void
+    {
+        $this->afterApplicationCreated(function () {
+            Passport::loadKeysFrom(self::KEYS);
+
+            @unlink(self::PUBLIC_KEY);
+            @unlink(self::PRIVATE_KEY);
+
+            $this->artisan('passport:keys');
+        });
+
+        $this->beforeApplicationDestroyed(function () {
+            @unlink(self::PUBLIC_KEY);
+            @unlink(self::PRIVATE_KEY);
+        });
+
+        parent::setUp();
+    }
 }
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,35 +4,8 @@ declare(strict_types=1);
 
 namespace N3XT0R\PassportModernScopes\Tests;
 
-use Illuminate\Foundation\Testing\LazilyRefreshDatabase;
-use Orchestra\Testbench\Concerns\WithWorkbench;
-use Illuminate\Contracts\Config\Repository;
-use Workbench\App\Models\User;
-use Laravel\Passport\Passport;
+use Orchestra\Testbench\TestCase as OrchestraTestCase;
 
-class TestCase extends DatabaseTestCase
+class TestCase extends OrchestraTestCase
 {
-    const string KEYS = __DIR__ . '/../keys';
-    const string PUBLIC_KEY = self::KEYS . '/oauth-public.key';
-    const string PRIVATE_KEY = self::KEYS . '/oauth-private.key';
-
-    protected function setUp(): void
-    {
-        $this->afterRefreshingDatabase();
-        $this->afterApplicationCreated(function () {
-            Passport::loadKeysFrom(self::KEYS);
-
-            @unlink(self::PUBLIC_KEY);
-            @unlink(self::PRIVATE_KEY);
-
-            $this->artisan('passport:keys');
-        });
-
-        $this->beforeApplicationDestroyed(function () {
-            @unlink(self::PUBLIC_KEY);
-            @unlink(self::PRIVATE_KEY);
-        });
-
-        parent::setUp();
-    }
 }
