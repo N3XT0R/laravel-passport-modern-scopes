66e1bd5f9cbab65af3af657cec40dccf412b1ff0
Ilya Beliaev <info@php-dev.info>
2025-12-31 13:30:14 +0100

    initial commit.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,9 +4,10 @@ declare(strict_types=1);
 
 namespace N3XT0R\PassportModernScopes\Tests;
 
+use Orchestra\Testbench\Concerns\WithWorkbench;
 use Orchestra\Testbench\TestCase as OrchestraTestCase;
 
 class TestCase extends OrchestraTestCase
 {
-
+    use WithWorkbench;
 }
