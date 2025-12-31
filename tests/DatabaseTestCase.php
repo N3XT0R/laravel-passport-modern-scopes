d8849c5247dc47f2ec6f4ecced3858ce9a2fd699
Ilya Beliaev <info@php-dev.info>
2025-12-31 13:43:48 +0100

    initial commit.

---
+++ tests/DatabaseTestCase.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace N3XT0R\PassportModernScopes\Tests;
+
+use Illuminate\Contracts\Config\Repository;
+use Illuminate\Foundation\Testing\LazilyRefreshDatabase;
+use Workbench\App\Models\User;
+
+class DatabaseTestCase extends TestCase
+{
+    use LazilyRefreshDatabase;
+
+    protected function defineEnvironment($app): void
+    {
+        $config = $app->make(Repository::class);
+
+        $config->set([
+            'auth.defaults.provider' => 'users',
+            'auth.providers.users.model' => User::class,
+            'auth.guards.api' => ['driver' => 'passport', 'provider' => 'users'],
+            'database.default' => 'testing',
+        ]);
+    }
+}
