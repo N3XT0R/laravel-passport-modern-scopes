e8655a05d7d61295532a6bf764880804ce2b1388
Ilya Beliaev <info@php-dev.info>
2025-12-31 13:55:06 +0100

    initial commit.

---
+++ tests/Unit/ExampleTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace N3XT0R\PassportModernScopes\Tests\Feature;
+
+use N3XT0R\PassportModernScopes\Tests\TestCase;
+
+class ExampleTest extends TestCase
+{
+    public function testExample(): void
+    {
+        $this->assertTrue(true);
+    }
+}
