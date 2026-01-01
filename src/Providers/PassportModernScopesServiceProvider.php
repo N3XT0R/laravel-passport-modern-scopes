04ecb5a2fb4a3dd4a27c302b3e55803121493652
Ilya Beliaev <info@php-dev.info>
2026-01-01 18:47:51 +0100

    initial commit

--- src/Providers/PassportModernScopesServiceProvider.php
+++ src/Providers/PassportModernScopesServiceProvider.php
@@ -7,6 +7,7 @@ namespace N3XT0R\PassportModernScopes\Providers;
 use Illuminate\Support\ServiceProvider;
 use N3XT0R\PassportModernScopes\Http\Middleware\ResolvePassportScopeAttributes;
 use N3XT0R\PassportModernScopes\Support\Middleware\GroupInjector;
+use Illuminate\Contracts\Http\Kernel;
 
 class PassportModernScopesServiceProvider extends ServiceProvider
 {
@@ -19,21 +20,28 @@ class PassportModernScopesServiceProvider extends ServiceProvider
             'passport-modern-scopes'
         );
 
-        $this->bootMiddleware();
+        $this->app->afterResolving(
+            Kernel::class,
+            function () {
+                $this->bootMiddleware();
+            }
+        );
     }
 
     protected function bootMiddleware(): void
     {
-        $this->app->bind(GroupInjector::class, GroupInjector::class);
+        $this->app->bind(GroupInjector::class);
         $this->app->make(GroupInjector::class)
             ->inject(
                 ResolvePassportScopeAttributes::class
             );
 
         if ($this->app['config']->get('passport-modern-scopes.auto_boot.enabled', false)) {
-            $this->app['router']->pushMiddlewareToGroup('api', ResolvePassportScopeAttributes::class);
-            $this->app->make('Illuminate\Contracts\Http\Kernel')
-                ->pushMiddleware(ResolvePassportScopeAttributes::class);
+            $router = $this->app['router'];
+            if (!array_key_exists('api', $router->getMiddlewareGroups())) {
+                $router->middlewareGroup('api', []);
+            }
+            $router->pushMiddlewareToGroup('api', ResolvePassportScopeAttributes::class);
         }
     }
 
@@ -43,5 +51,7 @@ class PassportModernScopesServiceProvider extends ServiceProvider
             __DIR__ . '/../../config/passport-modern-scopes.php',
             'passport-modern-scopes'
         );
+
+        $this->app->singleton(ResolvePassportScopeAttributes::class);
     }
 }
