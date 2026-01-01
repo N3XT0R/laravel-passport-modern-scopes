bec8827e73ea011d28f5b1ea24a8dc64f2f73dd7
Ilya Beliaev <info@php-dev.info>
2026-01-01 18:48:49 +0100

    initial commit

--- workbench/bootstrap/app.php
+++ workbench/bootstrap/app.php
@@ -9,11 +9,11 @@ use function Orchestra\Testbench\default_skeleton_path;
 return Application::configure(basePath: $APP_BASE_PATH ?? default_skeleton_path())
     ->withRouting(
         web: __DIR__ . '/../routes/web.php',
-        commands: __DIR__ . '/../routes/console.php',
         api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->api();
+        //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
