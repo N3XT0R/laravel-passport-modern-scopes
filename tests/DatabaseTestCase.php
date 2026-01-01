b4e71882acd39e5c5322cfd6d7d68cddb40044f2
Ilya Beliaev <info@php-dev.info>
2026-01-01 18:24:15 +0100

    initial commit

--- tests/DatabaseTestCase.php
+++ tests/DatabaseTestCase.php
@@ -6,8 +6,10 @@ namespace N3XT0R\PassportModernScopes\Tests;
 
 use Illuminate\Contracts\Config\Repository;
 use Illuminate\Foundation\Testing\LazilyRefreshDatabase;
+use Orchestra\Testbench\Attributes\WithMigration;
 use Workbench\App\Models\User;
 
+#[WithMigration]
 class DatabaseTestCase extends TestCase
 {
     use LazilyRefreshDatabase;
